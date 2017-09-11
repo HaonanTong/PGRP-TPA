a889d404fe4a7dbbc7839f5c3b10e30492521642
htong <htong@scorpius.ece.ncsu.edu>
2017-09-11 18:28:14 -0400

    Qualify

---
+++ Process2_ANan_DEGs.m
@@ -0,0 +1,23 @@
+Table_tmp = readtable('Profiles-ANan-DEGs.csv','ReadRowNames',true,'ReadVariableNames',true);
+RowName_tmp = Table_tmp.Properties.RowNames;
+subT = [];
+for j = 1 : length(RowName_tmp)
+    indx = regexp(RowName_tmp{j},'AT\dG\d{5}.1');
+    if ~isempty(indx)
+        subT=[subT; Table_tmp(j,:)];
+    end
+end
+
+%filter subT
+if ~isempty(subT)
+    RowName_subT = subT.Properties.RowNames;
+
+    for j = 1 : length(RowName_subT)
+        tmp = RowName_subT{j};
+        RowName_subT{j} = tmp(1:9);
+    end
+    subT.Properties.RowNames = RowName_subT;
+
+    % Wrtie  table
+    writetable(subT,'Profiles-ANan-DEGs-Filtered.csv','WriteRowNames',true,'WriteVariableNames',true);
+end
